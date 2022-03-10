5aceef2ea2629cbff02ea36a5855df786249fc4b
seblemariam <Seblemariam.sm@gmail.com>
2022-03-10 13:54:05 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * Description: The function print some text to the stdout
+ * Return: returns 0
+ */
+int main(void)
+{
+    puts("programming is like building a multilingual puzzle\n");
+    return (0)
+}
+
