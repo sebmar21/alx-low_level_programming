02a962faf9ce6df64aa89ab588e6449922079c43
seblemariam <Seblemariam.sm@gmail.com>
2022-03-10 14:23:39 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stido.h>
 
+/**
+ * main -main function
+ * Description: The function print some text to the stdout
+ * Return:returns void
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
