905d81e62a719b97bf531c705541ca8e1f7e9166
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 06:31:42 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
