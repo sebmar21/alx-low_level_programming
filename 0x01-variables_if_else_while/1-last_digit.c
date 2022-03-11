57367f889442fd2e631242a3f0aa0b27c8330842
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 13:18:55 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,7 @@
 
 /**
  * main - Entry point
- * Description: print the value of n status:
-                greater than, is zero and is not less than 6.
+ * Description: print the value of n status
  * Return: Always 0 (Success)
  */
 int main(void)
