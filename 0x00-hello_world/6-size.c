bc0aab7343519a56defb5cc1c6435ea5ef296aaa
seblemariam <Seblemariam.sm@gmail.com>
2022-03-10 16:27:25 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/** 
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	return (0);
+ 
+}
+
