8d28431540a8045e845dd33c52d2d97454d34e37
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 06:40:03 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /** 
- * main - Entry point
- * Description: using sizeof to print the size of various types.
+ * main -Entry point
+ * Description: Using sizeof to print the size of various types.
  * Return: Always 0 (Success)
  */
 int main(void)
