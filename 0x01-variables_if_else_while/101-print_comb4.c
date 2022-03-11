08c959e26a9731322b835fd5865592836871e760
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 15:40:24 -0800

    com 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-int didit1 = 0;
+int digit1 = 0;
 int digit2, digit3;
 while (digit1 <= 9)
 {
@@ -16,6 +16,7 @@ while (digit2 <= 9)
 {
 digit3 = 0;
 while (digit3 <= 9)
+{
 if (digit1 != digit2 &&
 digit1 < digit2 &&
 digit2 != digit3 &&
@@ -24,7 +25,7 @@ digit2 < digit3)
 putchar(digit1 + 48);
 putchar(digit2 + 48);
 puychar(digit3 + 48);
-if (digit1 + digit2 + digit3 !=24)
+if (digit1 + digit2 + digit3 != 24)
 {
 putchar(',');
 putchar(' ');
