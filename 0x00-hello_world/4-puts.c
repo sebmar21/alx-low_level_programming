1f22fdc5df3531b40f3af1c3983576737e7a94ae
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 05:27:13 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 /**
  * main - Entry point
- *
  * Description: A C program that prints with put function
- *
  * Return: Always 0 (Success) 
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
