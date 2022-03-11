4157e8981889f2b7554bbe3f319561d73d08ac57
seblemariam <Seblemariam.sm@gmail.com>
2022-03-11 15:01:11 -0800

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 int digit = 0;
-while (digit <= 9);
+while (digit <= 9)
 {
 putchar(digit + 48);
 if (digit != 9)
 {
-putchar (',');
-putchar (' ');
+putchar(',');
+putchar(' ');
 }
 ++digit;
 }
