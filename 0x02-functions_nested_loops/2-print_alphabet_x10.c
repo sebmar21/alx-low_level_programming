42d0d767ce1f2e5489ae7f197b1051f66bdbf6ee
seblemariam <Seblemariam.sm@gmail.com>
2022-03-15 11:26:26 -0700

    2-print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
