3b8038ee29fc91d0fca1946a74d99cd245318a49
seblemariam <Seblemariam.sm@gmail.com>
2022-03-16 01:31:40 -0700

    time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,8 +26,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else 
-				_putchar((prod / 10); + 48); /*get the first digit*/
-			_putchar((prod % 10); + 48); /*get the second digit/
+				_putchar((prod / 10) + 48); /*get the first digit*/
+			_putchar((prod % 10) + 48); /*get the second digit/
 		}
 		_putchar('\n');
          }
