f98158a34d6c5165b77dc57be5d340dd8081e189
seblemariam <Seblemariam.sm@gmail.com>
2022-03-16 01:47:53 -0700

    time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,11 @@
 void times_table(void)
 {
 	int num, multi, prod;
-	for (num = 0; num <= 9; ++num)
-	{
+	for ( num = 0; num <= 9; ++num )
+{
 		_putchar(48);
 		for (multi = 1; multi <= 9; ++multi)
-		{
+{
 			_putchar(',');
 			_putchar(' ');
 			prod = num * multi;
@@ -28,7 +28,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + 48);
 			_putchar((prod % 10) + 48);
-		}
+}
 		_putchar('\n');
-         }
+}
 }
