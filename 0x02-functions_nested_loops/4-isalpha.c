ae5d38bb2474bf6c25fb62bc8e31a1a4c6450c26
seblemariam <Seblemariam.sm@gmail.com>
2022-03-16 02:58:41 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,11 @@
 /**
  * _isalpha - checks if character is aletter
  *            both lowercase or uppercase
+ *
  * @c: take input from other functions.
- * Return: 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+ *
+ * Return: 1 is c if true else 0
+ * 
  */
 int _isalpha(int c)
 {
