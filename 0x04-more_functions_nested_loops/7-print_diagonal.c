906a66d52cb4ca0ce745265d7aeaf1f286d4e460
seblemariam <Seblemariam.sm@gmail.com>
2022-03-17 13:22:04 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,12 +18,9 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
-
 				_putchar(' ');
-
-			_putchar(92); /*is equal to '/' char*/
-
+		        _putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
-		}
-	}
+}
+}
 }
