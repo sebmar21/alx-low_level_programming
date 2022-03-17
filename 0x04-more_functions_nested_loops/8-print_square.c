1adb6376ea7ba7f4f81d583c0a424832075a5429
seblemariam <Seblemariam.sm@gmail.com>
2022-03-17 14:03:48 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,13 @@
 
 void print_square(int size)
 {
-	int row, column;
+        int row, column;
 
-	for (row = 1; row <= size; ++row)
-	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
-	}
+        for (row = 1; row <= size; ++row)
+        {
+                for (column = 1; column <= size; ++column)
+                        _putchar('#');
+                _putchar('\n');
+        }
 }
+
