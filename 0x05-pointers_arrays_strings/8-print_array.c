3e53afc668e05142f1513fc5236f8718d623644d
seblemariam <Seblemariam.sm@gmail.com>
2022-03-21 14:22:07 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n-1))
+		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
 		}
-		else if (i != (n-1))
+		else if (i != (n - 1))
 		{
 			printf("%d", a[i]);
 			printf(", ");
