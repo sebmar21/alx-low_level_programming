60c7673bd78a69d9f9820f2c43f99b4d3dc9e86b
seblemariam <Seblemariam.sm@gmail.com>
2022-03-21 12:24:40 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 /**
- * int _strlen - returns the length of a string
+ * _strlen - returns the length of a string
  *
  * @s: string parameter input
  *
  * Return: length of string
- */
+*/
 
 int _strlen(char *s)
 {
 	int counter;
 
-	while (counter = 0; *s != '\0'; ++s)
-			++counter;
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
 
-			return (0);
+	return (counter);
 }
