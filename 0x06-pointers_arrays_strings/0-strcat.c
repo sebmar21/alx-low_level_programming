191edbaf02af4fd6422573573550c020c2fd3dd9
seblemariam <Seblemariam.sm@gmail.com>
2022-03-24 07:57:11 -0700

    scrcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * _strcat - function that connect two strings.
+ * @dest: pointer to distination input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, x2;
+
+	x = 0;
+	/*find the size of dest array*/
+	while (dest[x]
+			x++;
+
+	/* iterate through each src array value without the null byte*/
+			for (x2 = 0; src[x2] ; x2++)
+/*append src[x2] to dest[x] while overwritting the null byte in dest*/
+			dest[x++] = src[x2];
+
+			return (dest);
+			}
