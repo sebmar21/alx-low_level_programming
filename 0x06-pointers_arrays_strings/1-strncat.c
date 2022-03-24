428f0d3ecfb287bacd97aca07fd82e501fe15871
seblemariam <Seblemariam.sm@gmail.com>
2022-03-24 08:33:28 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: most number of bytes from @src
  *
  * Return: @dest
- */
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -15,13 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 	c = 0;
 
 	/*find size of dest array*/
-	while (dset[c])
+	while (dest[c])
 		c++;
 
 	/**
 	 * src does not need to be null terminated
 	 * if it contains n or more bytes
-	 */
+	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
 	/*null terminate dest*/
