07003a2b6b90b53a66892534f288307174fbe855
seblemariam <Seblemariam.sm@gmail.com>
2022-03-28 09:02:14 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,32 +1,32 @@
 /**
  * _memset - a function that fills
- *           memmory with a constant byte
+ *         memory with a constant byte
  *
- *@s: input pointer to char type
+ * @s: input pointer to char type
  *    represents the pointer to the
  *    block of memory to fill
- *@b: input variable of char type
- *    represent the character to fills
- *@n; unsigned int variable
+ * @b: input variable of char type
+ *    represents the character to
+ *    fill s
+ * @n: unsigned int variable
  *    the number of bytes to be filled
  *
- *    Return: A pointer to the filled memory
- *            area @s
- */
+ * Return: A pointer to the filled memory
+ *         area @s
+*/
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	/**
 	 * declare an unsigned int
-	 * we are storing a
+	 * because we are storing a
 	 * value that will always be
 	 * non-negative (zero or positive)
-	 */
+	*/
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
 
 	return (s);
-
 }
