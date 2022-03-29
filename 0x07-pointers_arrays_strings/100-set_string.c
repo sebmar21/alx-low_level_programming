5a185221fed0c5f9cdae3b444a9b471a58209614
seblemariam <Seblemariam.sm@gmail.com>
2022-03-29 09:20:31 -0700

    string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,14 +2,13 @@
 
 /**
  * set_string - a function ...
- *
  * @s: the pointer of char
  * @to: the chaine
  *
- *Return: 1 or 0 
+ * Return: 1 or 0
  */
 
 void set_string(char **s, char *to)
 {
-	*s = *to
+	*s = to;
 }
