e9fceff4c01419e0da2f2b473e0e4b0a9c9fec9a
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 07:54:31 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,5 @@
+#include"main.h"
+
 /**
  * _sqrt_recursion - a function that
  *                   returns the natural square
@@ -15,5 +17,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (_sqrt_recursion(n + 1));
+		return (_sqrt_recursion(n - 1));
 }
