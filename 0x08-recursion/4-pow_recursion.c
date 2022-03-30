f24231da0aecd40800bf3064c83ecfe6cda5746a
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 06:18:30 -0700

    power is power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,9 +14,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	else if (y == 0)     /*x is the power of 0 is 1*/
 		return (1);
-	else if (y % 2 == 0)
+	else if (y % 2 == 0)   /*if y is an even number*/
 	{
 		even = _pow_recursion(x, y / 2);
 		return (even * even);
