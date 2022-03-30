179ed26e08b5ec250ea9b411eea6ae290d6d85a5
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 09:55:29 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,18 +5,19 @@
  *                   returns the natural square
  *                   root of anumber
  *@n: input number
+ *@i: input number
  *Return: the square root of x
  *
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n int i)
 {
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (0);
-	if (n == 0)
-		return (1);
+	if (n == 1)
+		return (n);
 	else
 		return (_sqrt_recursion(n - 1));
 }
