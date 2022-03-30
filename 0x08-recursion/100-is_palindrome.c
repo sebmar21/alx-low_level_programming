e9f545a296de08cd0d14dc5c7cc82de61cc4f422
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 09:25:17 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * is_palindrome - a function that returns 1
+ *                 if a string is
+ *                 a palindrome
+ *@s: string
+ *Return: Always 0 (Success)
+ */
+
+int is_palindrome(char *s)
+{
+	if (s != " ")
+		return (1);
+	else
+		return (0);
+}
