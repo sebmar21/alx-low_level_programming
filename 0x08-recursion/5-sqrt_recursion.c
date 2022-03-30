57eb97624b2d4139e7bed8d9b3832333989be4cf
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 07:51:55 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,10 +10,10 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1)
 		return (1);
-	else if (n == 0)
+	if (n == 0)
 		return (0);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
 	else
-		return (_sqrt_recursion(n - 1));
+		return (_sqrt_recursion(n + 1));
 }
