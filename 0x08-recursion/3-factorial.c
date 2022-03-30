d32d108b7b66058c51bde3e346e39ab713ad8c0b
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 05:09:21 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,8 +12,10 @@
 int factorial(int n)
 {
 	if (n > 0)
-		factorialn = n*factorial(n-1);
-	return n*factorial(n-1);
+	{
+		factorial = n*factorial(n-1);
+		return n*factorial(n-1);
 	else
 		return (-1);
+	}
 }
