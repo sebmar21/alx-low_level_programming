fa2b8c00397ec6e2a05ccc0bfb68a539289e9bce
seblemariam <Seblemariam.sm@gmail.com>
2022-03-30 00:19:17 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,13 @@
  * Return: nonnegative number (Success)
  */
  
-void _puts_recursion(const char *s)
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		putchar(*s);
 	_puts_recursion(s + 1);
 	}
         else
-		_putchar('\n');
+		putchar('\n');
 }
