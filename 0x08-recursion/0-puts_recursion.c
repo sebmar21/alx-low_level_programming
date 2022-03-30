576c4bc3d6f2f55785857de9fefa2a82954e6715
seblemariam <Seblemariam.sm@gmail.com>
2022-03-29 23:53:25 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts - a function that print a string
+ *         trailing new line
+ *@s: input string
+ *
+ * Return: nonnegative number (Success)
+ */
+ 
+void _puts_recursion(char *s)
+{
+	int puts(char *s);
+	return (0);
+}
