f9f1ee239f6b2e09f09a608782dddd9bfc32e544
seblemariam <Seblemariam.sm@gmail.com>
2022-04-01 15:41:53 -0700

    ebs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _abs - function that computes the absolute
+ *      value of the integer
+ * @n: takes integer type input for function
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+        if (n < 0)
+                n = (-1) * n;
+        return (n);
+}
