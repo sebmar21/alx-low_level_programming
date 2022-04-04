ecdb19dceed8c9c0d25f52a7483df8226f71882c
seblemariam <Seblemariam.sm@gmail.com>
2022-04-04 13:47:14 -0700

    commet

---
+++ 0x09-static_libraries/lib/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * _memset - a function ...
+ * @s: the chaine
+ * @b: the char
+ * @n: the int
+ *
+ * Return: 1 or 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
