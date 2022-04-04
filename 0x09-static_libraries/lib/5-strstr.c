fbd1a469263e4bc04b6d45ed9dc63b8a8d6b711c
seblemariam <Seblemariam.sm@gmail.com>
2022-04-04 14:40:31 -0700

    commet

---
+++ 0x09-static_libraries/lib/5-strstr.c
@@ -0,0 +1,37 @@
+#include <string.h>
+
+/**
+ * _strstr - a function ...
+ * @haystack: the chaine
+ * @needle: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
