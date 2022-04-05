ba2de8c167d1e9573f82e44d2db17a06d7c05193
seblemariam <Seblemariam.sm@gmail.com>
2022-04-05 10:55:46 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * alloc_grid - a function ...
+ * @width: the chaine
+ * @height:
+ *
+ * Return: 1 or 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **tableau2d;
+	int i, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	tableau2d = malloc(height * sizeof(int *));
+
+	if (tableau2d == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; ++i)
+	{
+		tableau2d[i] = malloc(width * sizeof(int));
+		if (tableau2d[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(tableau2d[i]);
+			free(tableau2d);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; ++i)
+		for (j = 0; j < width; j++)
+			tableau2d[i][j] = 0;
+
+	return (tableau2d);
+}
