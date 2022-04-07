2403ee277293344203e5aeee929adde8ae416f5b
seblemariam <Seblemariam.sm@gmail.com>
2022-04-07 07:13:51 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_cheked - a function allocates
+ *                  memory using malloc
+ *@b: amount of bytes
+ *Return: pointer to our new allocated memory
+ *        exit with 98 if malloc f ails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *new_mem;
+
+	new_mem = malloc(b);
+	if (new_mem == NULL)
+		exit(98);
+	return (new_mew);
+}
