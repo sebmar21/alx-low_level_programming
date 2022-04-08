79df266d1aa0f58ed2e20a5dd26238d774b48a97
seblemariam <Seblemariam.sm@gmail.com>
2022-04-08 11:50:19 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,9 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+        void *ptr = malloc(b);
 
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+        if (ptr == NULL)
+                exit(98);
+        return (ptr);
 }
