6f464c48d5941fb4a6a51d621d80adeb64d74155
seblemariam <Seblemariam.sm@gmail.com>
2022-04-13 08:21:03 -0700

    functions

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include<stddef.h>
+/**
+ * print_name - a function that print name
+ * @name: the chaine
+ * @f: the function
+ *
+ * Return: 1 or 0
+ * */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	(*f)(name);
+}
