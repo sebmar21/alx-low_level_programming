a1feac22feb042931cfc451d5f399072995180dc
seblemariam <Seblemariam.sm@gmail.com>
2022-04-13 08:40:44 -0700

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include<stddef.h>
-/**
- * print_name - a function ...
- * @name: the chaine
- * @f: the function
- *
- * Return: 1 or 0
- */
+#include "function_pointers.h"
 
+/**
+ * print_name - Prints a name
+ * @name: The name to prints
+ * @f: Pointer to function
+ *
+ * Return: Nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-		return;
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
