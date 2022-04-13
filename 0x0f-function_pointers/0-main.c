00e7091bb083eaefbd0fa20a1f931d815b72a56b
seblemariam <Seblemariam.sm@gmail.com>
2022-04-13 06:33:48 -0700

    function

---
+++ 0x0f-function_pointers/0-main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_uppercase(char *name)
+{
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
+    }
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
+    return (0);
+}
+julien@ubuntu:~/0x0e. Function pointers$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_name.c -o a
+julien@ubuntu:~/0x0e. Function pointers$ ./a 
+Hello, my name is Bob
+Hello, my uppercase name is BOB DYLAN
+julien@ubuntu:~/0x0e. Function pointers$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x0F-function_pointers
+File: 0-print_name.c
+   
+1. If you spend too much time thinking about a thing, you'll never get it done
+mandatory
+Write a function that executes a function given as a parameter on each element of an array.
+
+Prototype: void array_iterator(int *array, size_t size, void (*action)(int));
+where size is the size of the array
+and action is a pointer to the function you need to use
+julien@ubuntu:~/0x0e. Function pointers$ cat 1-main.c
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem(int elem)
+{
+    printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem_hex(int elem)
+{
+    printf("0x%x\n", elem);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[5] = {0, 98, 402, 1024, 4096};
+
+    array_iterator(array, 5, &print_elem);
+    array_iterator(array, 5, &print_elem_hex);
+    return (0);
+}
