77c16b30ca13c52129c58064cb94df5bc1c8e406
seblemariam <Seblemariam.sm@gmail.com>
2022-04-13 08:25:39 -0700

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include<stddef.h>
 /**
- * print_name - a function that print name
+ * print_name - a function ...
  * @name: the chaine
  * @f: the function
  *
  * Return: 1 or 0
- * */
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
