40a930b13089c4bf4431315863bac498dc6a9996
seblemariam <Seblemariam.sm@gmail.com>
2022-07-01 10:56:56 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 	unsigned long int i = 0;
-
 	new = malloc(sizeof(hash_table_t));
-
 	if (new == NULL)
 		return (NULL);
-
 	new->size = size;
 	new->array = malloc(sizeof(hash_table_t *) * size);
 	if (new->array == NULL)
